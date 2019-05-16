f52b7f3d77618c678baa91a2507eb07f2eec4344
J. Mark Stevens <github@jmarkstevens.com>
2019-05-16 08:23:54 -0700

    first run

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = 'Basic setup with watch'
+const title = 'Code Viewer 2019'
 
 ReactDOM.render(<div>{title}</div>, document.getElementById('react-app'))
--- wp/wp.config.js
+++ wp/wp.config.js
@@ -33,9 +33,5 @@ module.exports = {
   ],
   resolve: {
     extensions: ['*', '.css', '.js', '.jsx', '.scss']
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
   }
 }
