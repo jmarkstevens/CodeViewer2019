a77e227487feceb72a985d7424eccff8710c8653
J. Mark Stevens <github@jmarkstevens.com>
2019-05-17 06:41:50 -0700

    file component style

--- src/components/file/file-component.jsx
+++ src/components/file/file-component.jsx
@@ -1,17 +1,5 @@
+/* eslint-disable react/no-danger */
 import React from 'react'
-
-const FileViewSty = {
-  backgroundColor: '#001000',
-  color: '#ccc',
-  margin: '5px 20px',
-  overflow: 'auto',
-  width: '100%'
-}
-
-const FileCtrlSty = {
-  marginBottom: '5%',
-  width: '100%'
-}
 
 const FileView = ({ fileData }) => {
   const htmlDivSty = { width: '100%' }
@@ -19,10 +7,8 @@ const FileView = ({ fileData }) => {
   else htmlDivSty.overflow = 'auto'
   const fileHtml = { __html: fileData }
   return (
-    <div id="FileViewCtrlRenderSty" style={FileViewSty}>
-      <div id="FileCtrlSty" style={FileCtrlSty}>
-        <div dangerouslySetInnerHTML={fileHtml} style={htmlDivSty} />
-      </div>
+    <div className="file-view">
+      <div dangerouslySetInnerHTML={fileHtml} style={htmlDivSty} />
     </div>
   )
 }
